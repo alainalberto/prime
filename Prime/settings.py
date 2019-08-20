8f76c094e0546c0e88d128aeac492bad1fe0bcb5
alainalberto <alainalberto03@gmail.com>
2019-08-20 17:25:53 -0500

    Alain: Configuracion Servidor

--- Prime/settings.py
+++ Prime/settings.py
@@ -34,7 +34,7 @@ if socket.gethostname() == 'Ransel-PC' or socket.gethostname() == 'alain-HP-Note
     ALLOWED_HOSTS = []
 else:
     DEBUG = True
-    ALLOWED_HOSTS = ['159.65.250.237', 'www.prime1agency.com','prime1agency.com', 'http://prime1agency.com/']
+    ALLOWED_HOSTS = ['134.209.215.193', 'www.prime1agency.com','prime1agency.com', 'http://prime1agency.com/']
 
 
 # Application definition
